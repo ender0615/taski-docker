ec49caa67e64a9c335175508aa565f027c07d2af
Ратмир Хашагульгов <ender0615@mail.ru>
2024-06-28 23:25:52 +0300

    add

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -6,9 +6,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
-DEBUG = os.getenv('DEBUG', '').lower() in ('true', '1')
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'donatelo.zapto.org']
+ALLOWED_HOSTS = ['81.200.24.129', '127.0.0.1', 'localhost', 'donatelo.zapto.org']
 
 
 # Application definition
