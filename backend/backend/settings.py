71a24283e56268726569e7b247607df3187b6a9c
Ратмир Хашагульгов <ender0615@mail.ru>
2024-06-28 22:13:51 +0300

    add

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = os.getenv('DEBUG', '').lower() in ('true', '1')
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'donatelo.zapto.org']
 
 
 # Application definition
